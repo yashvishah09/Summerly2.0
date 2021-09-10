2e0018dbcc946d34ae2e037baa99cee1bde770db
Yashvi Shah <yashvishah1307@gmail.com>
2021-09-10 10:47:07 -0400

    Prepared

--- server.js
+++ server.js
@@ -22,6 +22,17 @@ mongoose
   .then(() => console.log('DB connection successful!'));
 
 const port = process.env.PORT || 5000;
+
+if(process.env.NODE_ENV === 'production'){
+
+  //Set static folder
+  app.use(express.static('frontend/build'));
+
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'frontend', 'build', 'index.html'))
+  })  
+}
+
 const server = app.listen(port, () => {
   console.log(`App running on port ${port}.`);
 });
